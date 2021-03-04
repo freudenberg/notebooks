2dd2baf81eaf18944b0090ec2b4f51d9ada2bfd5
Stefan Freudenberg <stefan@agaric.com>
2021-03-04 14:49:53 +0100

    Add function for loading demographic data

--- Todesursachen 1980-2019 Deutschland.py
+++ Todesursachen 1980-2019 Deutschland.py
@@ -26,6 +26,48 @@ cf.go_offline()
 # %%
 icd = ["Bestimmte infektiöse und parasitäre Krankheiten", "Neubildungen", "Krankheiten des Blutes u. der blutbildenden Organe", "Endokrine, Ernährungs- u. Stoffwechselkrankheiten", "Krankheiten des Kreislaufsystems", "Psychische und Verhaltensstörungen", "Krankheiten d. Nervensystems u. d. Sinnesorgane", "Krankheiten des Atmungssystems", "Krankheiten des Verdauungssystems", "Krankheiten der Haut und der Unterhaut", "Krankh. des Muskel-Skelett-Systems u. Bindegewebes", "Krankheiten des Urogenitalsystems", "Schwangerschaft, Geburt und Wochenbett", "Best.Zustände mit Ursprung in der Perinatalperiode", "Angeb. Fehlbildungen,Deformitäten,Chromosomenanom.", "Symptome und abnorme klinische und Laborbefunde", "Äußere Ursachen von Morbidität und Mortalität"]
 
+
+# %%
+def load_demographics(file):
+    with open(file, 'rb') as f:
+        df_ = pd.read_csv(
+            f,
+            dtype={'Deutsche': 'Int64', 'Ausländer': 'Int64'},
+            encoding='latin_1',
+            header=[0, 1], index_col=[0, 1],
+            na_values=['-'],
+            nrows=3480,
+            parse_dates=True,
+            sep=';',
+            skiprows=[0, 1, 2, 3, 4, 5]
+        )
+    df_['männlich'] = df_['Deutsche']['männlich'] + df_['Ausländer']['männlich']
+    df_['weiblich'] = df_['Deutsche']['weiblich'] + df_['Ausländer']['weiblich']
+    df_ = df_[['männlich', 'weiblich']].unstack(level=0).reindex(df_.index.get_level_values(1))
+    frames = {
+            "unter 1 Jahr": df_.iloc[0],
+            "1 bis unter 15 Jahre": df_.iloc[1:15].sum(),
+            "15 bis unter 20 Jahre": df_.iloc[15:20].sum(),
+            "20 bis unter 25 Jahre": df_.iloc[20:25].sum(),
+            "25 bis unter 30 Jahre": df_.iloc[25:30].sum(),
+            "30 bis unter 35 Jahre": df_.iloc[30:35].sum(),
+            "35 bis unter 40 Jahre": df_.iloc[35:40].sum(),
+            "40 bis unter 45 Jahre": df_.iloc[40:45].sum(),
+            "45 bis unter 50 Jahre": df_.iloc[45:50].sum(),
+            "50 bis unter 55 Jahre": df_.iloc[50:55].sum(),
+            "55 bis unter 60 Jahre": df_.iloc[55:60].sum(),
+            "60 bis unter 65 Jahre": df_.iloc[60:65].sum(),
+            "65 bis unter 70 Jahre": df_.iloc[65:70].sum(),
+            "70 bis unter 75 Jahre": df_.iloc[70:75].sum(),
+            "75 bis unter 80 Jahre": df_.iloc[75:80].sum(),
+            "80 bis unter 85 Jahre": df_.iloc[80:85].sum(),
+            "85 Jahre und mehr": df_.iloc[85]
+        }
+    df_ = pd.concat(frames.values(), keys=frames.keys()).unstack().T
+    df_.index = df_.index.year
+    return df_
+
+
 # %%
 with open('data/23211-0004.csv', 'rb') as f:
     df = pd.read_csv(
